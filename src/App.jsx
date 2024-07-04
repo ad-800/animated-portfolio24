bb3697e26a9a9fa9d3f04c50373d84b407702896
Adalyne Cummins <adalynecummins@gmail.com>
2024-07-03 20:37:20 -0400

    tests for framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './app.scss'
 import NavBar from './components/NavBar';
+import Test from './components/test.jsx'
 
 const App = () => {
   return <div>
-    <section><NavBar /></section>
+    {/* <section><NavBar /></section>
     <section>Parallax</section>
     <section>Services</section>
     <section>Parallax</section>
     <section>Portfolio_1</section>
     <section>Portfolio_2</section>
     <section>Portfolio_3</section>
-    <section>Contact</section>
+    <section>Contact</section> */}
+    <Test />
   </div>;
 };
 
---
+++ src/components/test.jsx
@@ -0,0 +1,44 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const Test = () => {
+  const [ open, setOpen ] = useState(false)
+
+  const variants = {
+    visible: (i) => ({opacity: 1, x: 200, transition: {delay: i * .3}}),
+    hidden: {opacity: 0},
+  }
+
+  const items = ['item1', 'item2', 'item3']
+
+  return (
+    <div className='course'>
+      {/* <motion.div
+        className='box'
+        variants={variants}
+        // initial="hidden"
+        // animate="visible"
+        // transition={{duration: 2}}
+        animate={open ? 'visible' : 'hidden'}
+        ></motion.div>
+        <button onClick={() => setOpen(prev => !prev)}>Click</button> */}
+        <motion.ul
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+        >
+          {items.map((item, i) => (
+            <motion.li
+              key={item}
+              variants={variants}
+              custom={i}
+            >
+              {item}
+            </motion.li>
+          ))}
+        </motion.ul>
+    </div>
+  )
+}
+
+export default Test;
