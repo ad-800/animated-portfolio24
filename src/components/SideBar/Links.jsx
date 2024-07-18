a7ce1708debf7886c426c82a82caee2c13358ee5
Adalyne Cummins <adalynecummins@gmail.com>
2024-07-17 21:03:04 -0400

    added svg and menu animation

--- src/components/SideBar/Links.jsx
+++ src/components/SideBar/Links.jsx
@@ -9,17 +9,45 @@ const Links = () => {
     'About'
   ]
 
+  const variants = {
+    open: {
+      transition: {
+        staggerChildren: 0.1,
+      }
+    },
+    closed: {
+      transition: {
+        staggerChildren: 0.05,
+        staggerDirection: -1,
+      }
+    }
+  }
+
+  const itemVariants = {
+    open: {
+      y: 0,
+      opacity: 1,
+    },
+    closed: {
+      y: 50,
+      opacity: 0,
+    }
+  }
+
   return (
-    <div className='links'>
+    <motion.div className='links' variants={variants}>
       {items.map(item => (
-        <a
+        <motion.a
           key={item}
           href={`#${item}`}
+          variants={itemVariants}
+          whileHover={{scale: 1.1}}
+          whileTap={{ scale: 0.95 }}
           >
             {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   )
 }
 
