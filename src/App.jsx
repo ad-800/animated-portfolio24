9bbe06c71974a9b9ab60310f052abdceb47b9e11
Adalyne Cummins <adalynecummins@gmail.com>
2024-07-23 20:25:29 -0400

    added parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './app.scss'
 import NavBar from './components/NavBar';
 import Hero from './components/Hero/Hero.jsx';
+import Parallax from './components/Parallax/Parallax.jsx';
 import Test from './components/test.jsx'
+import Services from './components/Services/services.jsx';
 
 const App = () => {
   return <div>
     <section id='Home'>
-      <NavBar />
-      < Hero />
+      <NavBar/>
+      <Hero/>
     </section>
-    <section id='Services'>Parallax</section>
-    <section>Services</section>
-    <section id='Portfolio'>Parallax</section>
+    <section id='Services'>
+      <Parallax type='services'/>
+    </section>
+    <section>
+      <Services/>
+    </section>
+    <section id='Portfolio'>
+      <Parallax type='portfolio'/>
+    </section>
     <section>Portfolio_1</section>
     <section>Portfolio_2</section>
     <section>Portfolio_3</section>
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,11 @@
+import './services.scss'
+
+const Services = () => {
+  return (
+    <div className='services'>
+      Services
+    </div>
+  )
+}
+
+export default Services
